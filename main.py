67764cada48c508893fc076a9aa72bb0ecb0a5f9
Cik232 <azamjon23202@gmail.com>
2024-11-08 16:59:32 +0500

    update

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def send_daily_request():
 
 @app.on_event("startup")
 def start_scheduler():
-    scheduler.add_job(send_daily_request, 'cron', hour=12, minute=0)  # Soat ishga tushadi
+    scheduler.add_job(send_daily_request, 'cron', hour=12, minute=5)  # Soat ishga tushadi
     scheduler.start()
 
 # Ilova yopilganda scheduler ham to'xtaydi
